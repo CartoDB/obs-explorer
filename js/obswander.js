e72bccf8077e9be7137bfd7a7de9336c4dec7efa
John Krauss <irving.krauss@gmail.com>
2016-06-01 16:10:36 -0400

    functionally uses metadata, removing the circle feature

--- js/obswander.js
+++ js/obswander.js
@@ -18,53 +18,53 @@ $( document ).ready(function() {
     y: 200
   };
 
-  var canvas = document.getElementById('cover');
-  ctx = canvas.getContext('2d');
+  //var canvas = document.getElementById('cover');
+  //ctx = canvas.getContext('2d');
 
-  canvas.height = $(window).height();
-  canvas.width = $(window).width();
-  circle.y = canvas.height / 2;
-  circle.x = canvas.width / 2;
+  //canvas.height = $(window).height();
+  //canvas.width = $(window).width();
+  //circle.y = canvas.height / 2;
+  //circle.x = canvas.width / 2;
 
-  ctx.fillStyle = "rgba(255,0,0,0)";
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-  var mask = document.createElement('canvas');
-  mask.width = canvas.width;
-  mask.height = canvas.height;
-  var maskCtx = mask.getContext('2d');
-  maskCtx.fillStyle = "rgba(0,0,0,0.6)";
-  maskCtx.fillRect(0, 0, mask.width, mask.height);
-  maskCtx.globalCompositeOperation = 'destination-out';
-  maskCtx.arc(circle.x, circle.y, circle.size, 0, 2*Math.PI);
-  maskCtx.fill();
-  ctx.drawImage(mask,0,0);
+  //ctx.fillStyle = "rgba(255,0,0,0)";
+  //ctx.fillRect(0, 0, canvas.width, canvas.height);
+  //var mask = document.createElement('canvas');
+  //mask.width = canvas.width;
+  //mask.height = canvas.height;
+  //var maskCtx = mask.getContext('2d');
+  //maskCtx.fillStyle = "rgba(0,0,0,0.6)";
+  //maskCtx.fillRect(0, 0, mask.width, mask.height);
+  //maskCtx.globalCompositeOperation = 'destination-out';
+  //maskCtx.arc(circle.x, circle.y, circle.size, 0, 2*Math.PI);
+  //maskCtx.fill();
+  //ctx.drawImage(mask,0,0);
 
-  cartodb.createVis('map', 'https://observatory.cartodb.com/api/v2/viz/d996e7b2-1c78-11e6-b921-0e31c9be1b51/viz.json', { zoom: 11, center: mapCenter})
+  cartodb.createVis('map', 'http://localhost:8000/viz.json', {
+    zoom: 11, center: mapCenter
+  })
     .done(function(map,layers){
       sublayer = layers[1].getSubLayer(0);
 
-      var nativeMap = map.getNativeMap()
+      var nativeMap = map.getNativeMap();
 
-      nativeMap.doubleClickZoom.disable();
-      nativeMap.scrollWheelZoom.disable();
-      nativeMap.boxZoom.disable();
-      nativeMap.touchZoom.disable();
-      nativeMap.keyboard.disable();
-      var el = $('.cartodb-zoom');
-      el.remove()
+      nativeMap.doubleClickZoom.enable();
+      nativeMap.scrollWheelZoom.enable();
+      nativeMap.boxZoom.enable();
+      nativeMap.touchZoom.enable();
+      nativeMap.keyboard.enable();
 
-      var circle = L.circle(mapCenter, 7200,{
-        fillColor: 'red',
-        fillOpacity: 0,
-        stroke: false,
-        weight: 14,
-        clickable: false
-      }).addTo(nativeMap);
+      //var circle = L.circle(mapCenter, 7200,{
+      //  fillColor: 'red',
+      //  fillOpacity: 0,
+      //  stroke: false,
+      //  weight: 14,
+      //  clickable: false
+      //}).addTo(nativeMap);
 
-      nativeMap.on('moveend', function(e){
-        circle.setLatLng(e.target.getCenter())
-        updateStats()
-      });
+      //nativeMap.on('moveend', function(e){
+      //  circle.setLatLng(e.target.getCenter());
+      //  updateStats();
+      //});
 
       var data_table = 'obs_1a098da56badf5f32e336002b0a81708c40d29cd'
       var geom_table = 'obs_6c1309a64d8f3e6986061f4d1ca7b57743e75e74'
@@ -72,44 +72,44 @@ $( document ).ready(function() {
       var data_geoid = 'geoid'
       var sql = new cartodb.SQL({ user: 'observatory', 'https': true });
 
-      var updateStats= function(){
-        column_name = selected_column
-        // bounds = nativeMap.getBounds()
+      //var updateStats= function(){
+      //  column_name = selected_column
+      //  // bounds = nativeMap.getBounds()
 
-        if (selected_agg_type=='sum'){
-          query = "SELECT sum(data.{{column_name}}) AS total_value, \
-                      sum( CASE WHEN geom.the_geom && ST_Transform(ST_Buffer(ST_Transform(CDB_LatLng({{center}}),3857),{{radius}}),4326) THEN data.{{column_name}} ELSE 0 END )  AS value\
-                      FROM {{data_table}} data, {{geom_table}} geom \
-                      WHERE data.{{data_geoid}} = geom.{{geom_geoid}}"
-        }
-        else if (selected_agg_type=='avg'){
-          query = "with avg_total as(\
-                    select avg({{column_name}}) as total from {{data_table}} \
-                  )\
-                  SELECT avg(data.{{column_name}}) AS value, \
-                      avg_total.total as total_value \
-                      FROM {{data_table}} data, {{geom_table}} geom avg_total \
-                      where \
-                    data.{{data_geoid}} = geom.{{geom_geoid}} \
-                    geom.the_geom && ST_Transform(ST_Buffer(ST_Transform(CDB_LatLng({{center}}),3857),{{radius}}),4326) \
-                    group by avg_total.total"
-        }
-        sql.execute(query, {
-          column_name: column_name,
-          data_table: data_table,
-          geom_table: geom_table,
-          data_geoid: data_geoid,
-          geom_geoid: geom_geoid,
-          agg_type: selected_agg_type,
-          center: [circle.getLatLng().lat, circle.getLatLng().lng].join(','),
-          radius: circle.getRadius()
-        })
-          .done(function(data) {
-            $(".js-figure").text(Math.floor(data.rows[0].value));
-        })
-      }
+      //  if (selected_agg_type=='sum'){
+      //    query = "SELECT sum(data.{{column_name}}) AS total_value, \
+      //                sum( CASE WHEN geom.the_geom && ST_Transform(ST_Buffer(ST_Transform(CDB_LatLng({{center}}),3857),{{radius}}),4326) THEN data.{{column_name}} ELSE 0 END )  AS value\
+      //                FROM {{data_table}} data, {{geom_table}} geom \
+      //                WHERE data.{{data_geoid}} = geom.{{geom_geoid}}"
+      //  }
+      //  else if (selected_agg_type=='avg'){
+      //    query = "with avg_total as(\
+      //              select avg({{column_name}}) as total from {{data_table}} \
+      //            )\
+      //            SELECT avg(data.{{column_name}}) AS value, \
+      //                avg_total.total as total_value \
+      //                FROM {{data_table}} data, {{geom_table}} geom avg_total \
+      //                where \
+      //              data.{{data_geoid}} = geom.{{geom_geoid}} \
+      //              geom.the_geom && ST_Transform(ST_Buffer(ST_Transform(CDB_LatLng({{center}}),3857),{{radius}}),4326) \
+      //              group by avg_total.total"
+      //  }
+      //  sql.execute(query, {
+      //    column_name: column_name,
+      //    data_table: data_table,
+      //    geom_table: geom_table,
+      //    data_geoid: data_geoid,
+      //    geom_geoid: geom_geoid,
+      //    agg_type: selected_agg_type,
+      //    center: [circle.getLatLng().lat, circle.getLatLng().lng].join(','),
+      //    radius: circle.getRadius()
+      //  })
+      //    .done(function(data) {
+      //      $(".js-figure").text(Math.floor(data.rows[0].value));
+      //  })
+      //}
 
-      updateStats();
+      //updateStats();
 
       /* read json */
       var subitemsMenu = function(e,f){
@@ -118,14 +118,18 @@ $( document ).ready(function() {
         var j = obj.filter_1;
         var subitems = [];
         $.each( j, function( k, val ) {
-          subitems.push( "<li><a href='#' data-agg='"+val.type+"' data-col='"+val.data_col+"' data-value='" + val.value + "'>" + val.label_1 + "</a></li>" );
+          if (val) {
+            subitems.push( "<li><a href='#' data-agg='" + val.type +
+                          "' data-col='" + val.data_col + "' data-value='" +
+                          val.value + "'>" + val.label_1 + "</a></li>" );
+          }
         });
 
         $( ".js-result-category" ).empty();
         $( ".box-result" ).empty();
         $( "<ul/>", {
           "class": "box-resultList js-result-category",
-          html: subitems.join( "" ),
+          html: subitems.join( "" )
         }).appendTo(".box-result");
         maxHeightList();
       };
@@ -137,23 +141,20 @@ $( document ).ready(function() {
           $('.box-container').toggleClass( "is-hidden" );
           $(".js-box-selectTitle").text($(this).text());
 
-          var column_name = $(this).attr('data-col')
-          sublayer.setSQL(
-            'with stats as( select max(data.'+column_name+'), min(data.'+column_name+') \
-                            from '+data_table+')\
-            select geom.the_geom_webmercator, \
-                   (data.'+column_name+'-stats.min)/(stats.max-stats.min) as val \
-            from stats, ' + data_table + 'data, ' + geom_table + 'geom \
-            where data.' + data_geoid + ' = geom.' + geom_geoid);
+          var column_name = $(this).attr('data-col');
 
+          sublayer.setSQL('with stats as( select max('+column_name+'), min('+column_name+') \
+                            from '+data_table+')\
+            select data.cartodb_id, geom.the_geom_webmercator, \
+                   (data.'+column_name+'-stats.min)/(stats.max-stats.min) as val \
+            from stats, ' + data_table + ' data, ' + geom_table + ' geom \
+            where data.' + data_geoid + ' = geom.' + geom_geoid);
           var css = sublayer.getCartoCSS();
           sublayer.setCartoCSS(css);
 
-          selected_column    = $(this).attr('data-col')
-          selected_agg_type  = $(this).attr('data-agg')
-          updateStats();
-
-
+          selected_column    = $(this).attr('data-col');
+          selected_agg_type  = $(this).attr('data-agg');
+          //updateStats();
         });
       };
       var scrollFunction = function(){
@@ -176,6 +177,7 @@ $( document ).ready(function() {
                          AND ctag.tag_id = t.id \
                          AND c.id = ctable.column_id \
                          AND ctable.table_id = tab.id \
+                         and c.aggregate ILIKE \'SUM\' \
                          AND tab.tablename = \'' + data_table + '\' \
                      ) AS filter_1 \
                      FROM obs_tag t \
@@ -184,8 +186,14 @@ $( document ).ready(function() {
 
       $.getJSON( 'http://observatory.cartodb.com/api/v2/sql', {
         q: query
-      }, function( data ) {
-        data = data.rows;
+      }, function( rawdata ) {
+        var data = [];
+        for (var i = 0; i < rawdata.rows.length; i += 1) {
+            row = rawdata.rows[i];
+            if (row.filter_1) {
+                data.push(row);
+            }
+        }
         var items = [];
         $.each( data, function( key, val ) {
             if (items.length == 0) {
@@ -200,8 +208,14 @@ $( document ).ready(function() {
         }).appendTo( ".box-nav" );
         subitemsMenu($( ".box-navNavigation a" ), data);
       })
-        .done(function(data){
-          data = data.rows;
+        .done(function(rawdata){
+          var data = [];
+          for (var i = 0; i < rawdata.rows.length; i += 1) {
+              row = rawdata.rows[i];
+              if (row.filter_1) {
+                  data.push(row);
+              }
+          }
           $( ".box-navNavigation a" ).on( "click", function() {
             var txt = $(this).text();
             $(".js-box-input").text(txt);
@@ -214,7 +228,6 @@ $( document ).ready(function() {
               var txtDown = $(this).text();
               txtDown = txtDown.toLowerCase();
               var dest = txtDown;
-              dest = dest.split(" ").join("");
               dest = dest.split(" ").join("");
               if($( ".box-icon svg" ).hasClass(dest)) {
                 $( ".box-icon svg."+dest ).show();
